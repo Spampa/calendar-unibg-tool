24c8ea36ef4ab847c060031a2c2431403fe3287c
Spampa <nicolo.spampa@gmail.com>
2025-02-20 20:04:46 +0100

    added policy link in home page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function Login(){
     return(
@@ -10,6 +11,10 @@ export default function Login(){
                 <h1 className="text-4xl font-bold">UNIBG Calendar Tool</h1>
                 <p>Abbiamo bisogno della tua autorizzazione Google per applicare il calendario 😊</p>
                 <Button onClick={() => signIn("google", { callbackUrl: "/" })}>Accedi Con Google</Button>
+            </div>
+
+            <div className="absolute bottom-0 w-full text-center p-3 text-sm">
+                Utilizzando UNIBG Calendar Tool confermi di avere letto l&apos;<Link href={"/policy"} className="underline">informativa sulla privacy.</Link>
             </div>
         </main>
     )
