d186308c0d09cf982a9dd02f89e29f435577ce13
Spampa <nicolo.spampa@gmail.com>
2025-02-24 18:58:01 +0100

    landing page updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,14 +7,15 @@ import FileUpload from "../_components/fileUpload";
 import EditTable from "../_components/editTable";
 import { redirect } from "next/navigation";
 import { useRef, useEffect, useState } from "react";
+import Link from "next/link";
 
 //types
 import { Subject, Event } from "@/types"
 
 export default function Home() {
   const { status, update } = useSession();
-  const [ calendar, setCalendar] = useState<Array<Event>>([]);
-  const [ subjects, setSubjects ] = useState<Array<Subject>>([]);
+  const [calendar, setCalendar] = useState<Array<Event>>([]);
+  const [subjects, setSubjects] = useState<Array<Subject>>([]);
 
   const updateRef = useRef(update);
 
@@ -24,11 +25,14 @@ export default function Home() {
 
   if (status === "authenticated") {
     return (
-      <main className=" container mx-auto p-6 flex flex-col items-center gap-6">
+      <main className="container mx-auto p-6 flex flex-col items-center gap-6 pb-20">
         <UserInfoCard />
         <Explain />
-        <FileUpload setCalendar={setCalendar} setSubjects={setSubjects}/>
+        <FileUpload setCalendar={setCalendar} setSubjects={setSubjects} />
         <EditTable calendar={calendar} subjects={subjects} setSubjects={setSubjects} />
+        <div className="fixed bottom-0 w-full text-center p-3 text-sm bg-background">
+          Utilizzando UNIBG Calendar Tool confermi di avere letto l&apos;<Link href={"/policy"} className="underline">informativa sulla privacy.</Link>
+        </div>
       </main>
     )
   } else {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { buttonVariants } from "@/components/ui/button"
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { MoveUpRight, ChevronRight } from "lucide-react";
 
 export default function Landing() {
     const { status } = useSession();
@@ -13,17 +14,20 @@ export default function Landing() {
     }
     else {
         return (
-            <main className="flex place-content-center h-screen bg-gradient-to-r from-background via-30% via-blue-950 to-background">
-                <div className="flex flex-col gap-4 h-full place-content-center items-center text-center">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-4xl font-bold">UNIBG Calendar Tool</h1>
-                        <p>Carica il tuo file Excel e sincronizza gli orari su <b>Google Calendar</b> in un clic.</p>
+            <main className="flex flex-col items-center gap-14 h-screen p-5 md:pt-32 mx-auto bg-background">
+                <div className="flex flex-col gap-14  md:w-2/3 xl:w-1/3">
+                    <div className="flex flex-col gap-8 items-center">
+                        <h1 className="text-4xl font-bold text-center">Introducing UNIBG Calendar Tool</h1>
+                        <div className="flex flex-row gap-2">
+                            <Link href={"/login"} className={buttonVariants()}>Prova Calendar Tool<MoveUpRight /></Link>
+                            <Link href={"/policy"} className={buttonVariants({ variant: "outline" })}>Informativa sulla Privacy<ChevronRight /></Link>
+                        </div>
                     </div>
-                    <Link href={"/login"} className={buttonVariants()}>Inizia Subito!</Link>
-                </div>
-
-                <div className="absolute bottom-0 w-full text-center p-3 text-sm">
-                    Utilizzando UNIBG Calendar Tool confermi di avere letto l&apos;<Link href={"/policy"} className="underline">informativa sulla privacy.</Link>
+                    <div className="border border-b-1"></div>
+                    <div className="flex flex-col items-center w-full gap-2">
+                        <p>UNIBG Calendar Calendar Tool è uno strumento online dedicato agli studenti e al personale dell'Università di Bergamo, progettato per semplificare la gestione del calendario accademico. Permette di convertire file Excel contenenti orari delle lezioni ed eventi universitari in un formato compatibile con Google Calendar, facilitando l'importazione e la sincronizzazione.</p>
+                        <p>Per offrire un'esperienza personalizzata e consentire l'importazione diretta su Google Calendar, richiediamo l'autenticazione tramite Google OAuth. L'accesso ci permette esclusivamente di creare e gestire eventi nel tuo calendario, senza raccogliere o condividere dati personali.</p>
+                    </div>
                 </div>
             </main>
         )
